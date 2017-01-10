3e24c678fa64db80a43ac3e9e689f8be5bae4376
Anton Stepan <info@antonstepan.it>
2017-01-10 19:35:14 +0100

    dependencies for redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Routes from './router';
+import { AppContainer } from 'react-hot-loader';
+import { browserHistory } from 'react-router';
+
+import configureStore from './store';
+import Root from './root';
+
+// import Routes from './router';
 import 'assets/style.css';
 
-const App = () => {
-  return (
-    <Routes />
-  );
-};
+const store = configureStore();
+const rootElement = document.getElementById('root');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function render(Root) {
+  ReactDOM.render(
+    <AppContainer>
+      <Root history={browserHistory} store={store} />
+    </AppContainer>,
+    rootElement
+  );
+}
+
+if (module.hot) {
+  module.hot.accept('./views/root', () => {
+    render(require('./views/root').default);
+  });
+}
+
+render(Root);
+
+// const App = () => {
+//   return (
+//     <Routes />
+//   );
+// };
+
+// ReactDOM.render(<App />, document.getElementById('root'));
