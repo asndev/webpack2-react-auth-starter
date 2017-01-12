03341c9e2ac629bd747bea700de820ce8a68b8d8
Anton Stepan <info@antonstepan.it>
2017-01-12 21:17:44 +0100

    ^.^

--- src/store/firebase/config.js
+++
@@ -1,8 +0,0 @@
-
-// firebase config can be retrieved from the firebase console
-export const firebaseConfig = {
-  apiKey: 'xxx',
-  authDomain: 'xxx.firebaseapp.com',
-  databaseURL: 'https://xxx.firebaseio.com',
-  storageBucket: 'xxx.appspot.com'
-};
