f3faff66fcb8152bf0976aeaf7399d64f43672df
Anton Stepan <info@antonstepan.it>
2017-01-12 21:45:03 +0100

    if this works, lol

--- karma.conf.js
+++ karma.conf.js
@@ -10,9 +10,21 @@ module.exports = (config) => {
     logLevel: config.LOG_INFO,
     singleRun: false,
     browsers: ['Chrome'],
+    customLaunchers: {
+      // tell TravisCI to use chromium when testing
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
     webpack: require('./webpack.config'),
     webpackServer: {
       noInfo: true
     }
   });
+
+  // Detect if this is TravisCI running the tests and tell it to use chromium
+  if (process.env.TRAVIS) {
+      config.browsers = ['Chrome_travis_ci'];
+  }
 };
