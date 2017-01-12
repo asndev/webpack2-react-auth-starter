f7a7899140f6acf9b203d72d79ae0db95a1dc36d
Anton Stepan <info@antonstepan.it>
2017-01-12 21:21:00 +0100

    dunno

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,6 +1,6 @@
 import {authSagas} from './auth';
 
-// Register all sagas which will be 'run' within the redux store
+// Register all sagas which will be 'run' within the saga middleware
 export default function* sagas() {
   yield [...authSagas];
 }
