2d34e980b4280771710911d60ed1d69fb38365b2
Anton Stepan <info@antonstepan.it>
2017-01-13 14:18:23 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
-
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
+
 const DefinePlugin = require('webpack/lib/DefinePlugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HotModule = require('webpack/lib/HotModuleReplacementPlugin');
