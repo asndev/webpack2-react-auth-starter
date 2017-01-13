f7129f41ab4bb3a96582bc45c523e95d2c6a40dd
asndev <stepan@patronas.com>
2017-01-13 10:35:46 +0100

    forgot to add the config ^.^

---
+++ src/store/firebase/config.js
@@ -0,0 +1,8 @@
+
+// firebase config can be retrieved from the firebase console
+export const firebaseConfig = {
+  apiKey: 'AIzaSyD8ofkaOlcaI2Xuz12ugvh5AQoLfBbgtPg',
+  authDomain: 'howmuch-9ee50.firebaseapp.com',
+  databaseURL: 'https://howmuch-9ee50.firebaseio.com',
+  storageBucket: 'howmuch-9ee50.appspot.com'
+};
